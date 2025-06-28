20b23237191fb877ab51025f546f4079098889f3
M-Vietinghoff <michaelavietinghoff@Gmail.com>
2025-06-28 21:50:02 +0000

    Formatting, Styling and Adjusting Event Page

--- my-app/src/pages/UpcomingEvents.jsx
+++ my-app/src/pages/UpcomingEvents.jsx
@@ -13,28 +13,31 @@ export default function UpcomingEvents() {
   )
     return (
       <div>
-        <div className="EventSearch">
-          <h2 className="text-2xl font-bold">Events page coming soon...</h2>
-          <input type="text" placeholder="Search.." />
-          <h3 className="text-2xl font-bold">Search Filter placeholder</h3>
-        </div>
+        
+      <div className="EventSearch">
+        <input type="text" placeholder="Search Location" className="searchInput" />
 
-        <div className="EventsContainer" style={{ height: "200px", overflowY: "auto"}}>
-         <ol onClick={() => scrollSection(Event1)} ref={Event1} className="Event1">
+        <div className="placeholderRow">
+         <h3 className="text-2xl font-bold">Distance Place Holder</h3>
+          <h3 className="text-2xl font-bold">Keywords Placeholder</h3>
+        </div>
+      </div>
+        <div className="EventsContainer" style={{ height: "500px", overflowY: "auto"}}>
+         <ul onClick={() => scrollSection(Event1)} ref={Event1} className="Event1">
             <li className="Name1">Event Name</li>
             <li className="Address1">Address: 6050 University Avenue, Halifax, NS B3H</li>
             <li className="Date1">Date: 5:00PM on June 29th</li>
-          </ol>
-         <ol onClick={() => scrollSection(Event2)} ref={Event2} className="Event2">
+          </ul>
+         <ul onClick={() => scrollSection(Event2)} ref={Event2} className="Event2">
             <li className="Name2">Event Name2</li>
             <li className="Address1">Address: 6050 University Avenue, Halifax, NS B3H</li>
             <li className="Date1">Date: 8:00PM on June 29th</li>
-          </ol>
-         <ol onClick={() => scrollSection(Event3)} ref={Event3} className="Event3">
+          </ul>
+         <ul onClick={() => scrollSection(Event3)} ref={Event3} className="Event3">
             <li className="Name3">Event Name3</li>
            <li className="Address1">Address: 6050 University Avenue, Halifax, NS B3H</li>
            <li className="Date1">Date: 10:00PM on June 29th</li>
-          </ol>
+          </ul>
         </div>
      </div>
     );
